471ab941877fd8b6741ed271fe6c7c6904f2ea5f
yaguarnecro <yaguarnecro@gmail.com>
2021-01-15 12:05:28 -0500

    fixing namespace on routes and view

--- prueba/bibliocrud/controller/page/View.php
+++ prueba/bibliocrud/controller/page/View.php
@@ -1,8 +1,5 @@
-<?php /**
- * Copyright © 2016 Magento. All rights reserved.
- * See COPYING.txt for license details.
- */
-namespace Learning\bibliocrud\Controller\Page;
+<?php 
+namespace prueba\bibliocrud\Controller\Page;
 class View extends \Magento\Framework\App\Action\Action
 {
     /**
