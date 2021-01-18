b1909b686aec1e7be5fb90ea6eede514f5c6f4d7
yaguarnecro <yaguarnecro@gmail.com>
2021-01-18 17:45:46 -0500

    fixing index.php from controler, making a route

--- prueba/bibliocrud/controller/Index/index.php
+++ prueba/bibliocrud/controller/Index/index.php
@@ -1,15 +1,11 @@
 <?php 
 namespace prueba\bibliocrud\Controller\Index;
+class Index extends Magento\Framework\App\Action\Action
 
-use Magento\Framework\App\Action\Action;
-use Magento\Framework\Controller\ResultFactory;
-
-
-class Index extends Action
 {
     public function excecute()
     {
-        $page = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
-        return $page;
+        echo '<p>You dit it! ? </p>';
+        var_dump(_METHOD_);
     }
 }
